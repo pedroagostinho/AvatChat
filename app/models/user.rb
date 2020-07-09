e43a65514194ffc77d98665fdcd4cba17c4e4a21
Pedro Agostinho <pedroagosti@gmail.com>
2020-07-09 16:37:43 +0100

    AvatChat setup in rails 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
+require "open-uri"
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :validate_photo
+
+  validates :name, presence: true
+
+  has_one_attached :photo
+
+  def validate_photo
+    unless photo.attached?
+      file = URI.open(Faker::Avatar.image)
+      photo.attach(io: file, filename: "#{name}.jpg", content_type: 'image/jpg')
+
+      save!
+    end
+  end
 end
