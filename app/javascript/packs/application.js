1c41d58856e47692255881a4fe7ff9e449d5535e
Pedro Agostinho <pedroagosti@gmail.com>
2020-07-12 19:05:20 +0100

    Action Cable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,9 +27,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initChatroomCable } from '../channels/chatroom_channel';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   AOS.init();
+  initChatroomCable();
 });
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index]
+
+  resources :chatrooms, only: [:show] do
+    resources :messages, only: :create
+  end
 end
